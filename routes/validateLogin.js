3b2bdbeefb10dd4b20f15035effd73c254334909
Francisco Perella-Holfeld <68356602+fperellaholfeld@users.noreply.github.com>
2021-12-05 15:42:47 -0800

    added lab work

---
+++ routes/validateLogin.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../auth");
+const sql = require("mssql");
+
+router.post("/", function (req, res) {
+  // Have to preserve async context since we make an async call
+  // to the database in the validateLogin function.
+  (async () => {
+    let authenticatedUser = await validateLogin(req);
+    if (authenticatedUser) {
+      res.redirect("/");
+    } else {
+      res.redirect("/login");
+    }
+  })();
+});
+
+async function validateLogin(req) {
+  if (!req.body || !req.body.username || !req.body.password) {
+    return false;
+  }
+
+  let username = req.body.username;
+  let password = req.body.password;
+  let authenticatedUser = await (async function () {
+    try {
+      let pool = await sql.connect(dbConfig);
+
+      // TODO: Check if userId and password match some customer account.
+      // If so, set authenticatedUser to be the username.
+      let sqlPass =
+        "USE tempdb; SELECT password FROM customer WHERE userid=@username AND password=@password";
+
+      let results = await pool
+        .request()
+        .input("username", sql.VarChar, username)
+        .input("password", sql.VarChar, password)
+        .query(sqlPass);
+      console.dir(results);
+      if (results.recordset[0]) {
+        req.session.authenticatedUser = username;
+        return true;
+      } else {
+        req.session.loginMessage = "Invalid Login Credentials";
+        return false;
+      }
+    } catch (err) {
+      console.dir(err);
+      return false;
+    }
+  })();
+
+  return authenticatedUser;
+}
+
+module.exports = router;
