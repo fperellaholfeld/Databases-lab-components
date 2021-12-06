aa14c6467f98526337f0bbceec0f916b5d28fd35
Francisco Perella-Holfeld <68356602+fperellaholfeld@users.noreply.github.com>
2021-12-05 17:07:14 -0800

    updated some files

--- routes/listprod.js
+++ routes/listprod.js
@@ -1,118 +1,149 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const sql = require('mssql');
-const moment = require('moment');
+const sql = require("mssql");
+const moment = require("moment");
 
-router.get('/', function(req, res, next) {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<title>Edible Textbooks Orders List</title>');
-    res.write('<header><nav><ul><li><a href="/"> Home </a></li><li><a href="/listprod"> Find Products </a></li><li><a href="/showcart"> Your Cart </a></li><li> <a href="/listorder"> Past orders </a></li></ul></nav></header>');
- 
-    res.write('<style type=text/css> header {background-color: white; position: static; left: 0; right: 0;  top: 5px;  height: 30px;  display: flex;  align-items: center;  }  header * {  display: inline;  }  header li {  margin: 20px;  }  header li a {  color: blue;  text-decoration: none;  }  </style>');
-    res.write('<h1 align="center"><font face="normal" color="#cf0921">Tasty Texts</font></h1><hr>');
-     res.write('<h1><font face="" color="#db2323">Enter name of product:</font></h1>');
-   
-    //get categories drop down menu
-    res.write('<form method ="get" action="listprod">');
-    res.write('<select class="selectpicker" name = "catName">');
-    res.write('<optgroup label = "Category">');
-    res.write('<option value=""> All </option>');
-    res.write('<option> Beverages </option>');
-    res.write('<option> Condiments </option>');
-    res.write('<option> Dairy Products </option>');
-    res.write('<option> Produce </option>');
-    res.write('<option> Meat/Poultry </option>');
-    res.write('<option> Seafood </option>');
-    res.write('<option> Confections </option>');
-    res.write('<option> Grains/Cereals </option>');
- 
-    res.write('</optgroup>')
+router.get("/", function (req, res, next) {
+  res.setHeader("Content-Type", "text/html");
+  res.write("<title>Edible Textbooks Orders List</title>");
+  res.write(
+    '<header><nav><ul><li><a href="/"> Home </a></li><li><a href="/listprod"> Find Products </a></li><li><a href="/showcart"> Your Cart </a></li><li> <a href="/listorder"> Past orders </a></li></ul></nav></header>'
+  );
 
-  
-    //get search bar
-    res.write('<form method="get" action="listprod">');
-    res.write('<input type="text" name="pName" size="50">');
-    res.write('<input type="submit" value="Submit"><input type="reset" value="Reset">');
-    res.write('</form>');
-    
-    res.write('</select>');
-    res.write('</form>');
+  res.write(
+    "<style type=text/css> header {background-color: white; position: static; left: 0; right: 0;  top: 5px;  height: 30px;  display: flex;  align-items: center;  }  header * {  display: inline;  }  header li {  margin: 20px;  }  header li a {  color: blue;  text-decoration: none;  }  </style>"
+  );
+  res.write(
+    '<h1 align="center"><font face="normal" color="#cf0921">Tasty Texts</font></h1><hr>'
+  );
+  res.write(
+    '<h1><font face="" color="#db2323">Enter name of product:</font></h1>'
+  );
 
-      (async function() {
-        try {
+  //get categories drop down menu
+  res.write('<form method ="get" action="listprod">');
+  res.write('<select class="selectpicker" name = "catName">');
+  res.write('<optgroup label = "Course Category">');
+  res.write('<option value=""> All </option>');
+  res.write("<option> BIOL </option>");
+  res.write("<option> MATH </option>");
+  res.write("<option> APSC </option>");
+  res.write("<option> COSC </option>");
+  res.write("<option> POLI </option>");
+  res.write("<option> WRLD </option>");
 
-            let pool = await sql.connect(dbConfig);
-                       
-            let results = false;
-            let sqlQuery =  "USE tempdb; SELECT productId, productName, productPrice, categoryName FROM product JOIN category ON product.categoryId=category.categoryId ";
+  res.write("</optgroup>");
 
-          if (req.query.pName && req.query.catName) {
-            sqlQuery = sqlQuery + "WHERE productName LIKE @productName AND categoryName LIKE @categoryName";
-            results = await pool.request()
-                .input('productName', sql.VarChar, "%" + req.query.pName + "%") 
-                .input('categoryName', sql.VarChar, "%" + req.query.catName + "%") 
-                .query(sqlQuery);
-           } else if (req.query.pName) {
-            sqlQuery = sqlQuery + "WHERE productName LIKE @productName";
-            results = await pool.request()
-                .input('productName', sql.VarChar, "%" + req.query.pName + "%") 
-                .query(sqlQuery);
-           } else if (req.query.catName) {
-            sqlQuery = sqlQuery + "WHERE categoryName LIKE @categoryName";
-            results = await pool.request()
-                .input('categoryName', sql.VarChar, "%" + req.query.catName + "%") 
-                .query(sqlQuery);
-           } else {
-            results = await pool.request()
-                .query(sqlQuery);
-        }
-                 res.write('<table border="1"><tr><th align="left"><font face="Helvetica">Add to Cart</th><th align="left"><font face="Helvetica">Product Id</th><th align="left"><font face="Helvetica">Product Name</th><th align="left"><font face="Helvetica">Product Price</th><th align="left"><font face="Helvetica">Category Name</th></tr>');
-                for(let i = 0; i <results.recordset.length; i++) {
-                    let searchRes = results.recordset[i];
-                    let pId = searchRes.productId;
-                    let prName = searchRes.productName;
-                    let prPrice = searchRes.productPrice;
-                    let caName = searchRes.categoryName;
-                    let color = 'black';
-                    if(caName == 'Beverages')
-                        color = '#943131';
-                    if(caName == 'Condiments')
-                        color = '#ad8832';
-                    if(caName == 'Produce')
-                        color = '#63ad32';
-                    if(caName == 'Meat/Poultry')
-                        color = '#33917b';
-                    if(caName == 'Seafood')
-                        color = '#338891';
-                    if(caName == 'Dairy Products')
-                        color = '#334c91';
-                    if(caName == 'Confections')
-                        color = '#543391';
-                    if(caName == 'Grains/Cereals')
-                        color = '#913384';
-                        res.write('<tr><td>' + '<font face="Helvetica"><a href="addcart?id='+pId+'&name='+prName+'&price='+prPrice+'">Add to Cart</a></font>'  + '</td><td><font face="Helvetica" color="'+color+'">' + pId + '</font></td><td><font face="Helvetica" color="'+color+'">' + '<a href="product?id='+pId+'&name='+prName+'&price='+prPrice+'">' + prName + '</a></font>' + '</font></td><td><font face="Helvetica" color="'+color+'">' + "$" + prPrice + '</font></td><td><font face="Helvetica" color="'+color+'">' + caName + '</font></td></tr>');
-           
-                }
-                 
-                 res.write("</table>");
-          
-              
+  //get search bar
+  res.write('<form method="get" action="listprod">');
+  res.write('<input type="text" name="pName" size="50">');
+  res.write(
+    '<input type="submit" value="Submit"><input type="reset" value="Reset">'
+  );
+  res.write("</form>");
 
-            res.end();
-        } catch(err) {
-            console.dir(err);
-            res.end();
-        }
-    })();
+  res.write("</select>");
+  res.write("</form>");
 
+  (async function () {
+    try {
+      let pool = await sql.connect(dbConfig);
 
-   
+      let results = false;
+      let sqlQuery =
+        "USE tempdb; SELECT productId, productName, productPrice, categoryName FROM product JOIN category ON product.categoryId=category.categoryId ";
+
+      if (req.query.pName && req.query.catName) {
+        sqlQuery =
+          sqlQuery +
+          "WHERE productName LIKE @productName AND categoryName LIKE @categoryName";
+        results = await pool
+          .request()
+          .input("productName", sql.VarChar, "%" + req.query.pName + "%")
+          .input("categoryName", sql.VarChar, "%" + req.query.catName + "%")
+          .query(sqlQuery);
+      } else if (req.query.pName) {
+        sqlQuery = sqlQuery + "WHERE productName LIKE @productName";
+        results = await pool
+          .request()
+          .input("productName", sql.VarChar, "%" + req.query.pName + "%")
+          .query(sqlQuery);
+      } else if (req.query.catName) {
+        sqlQuery = sqlQuery + "WHERE categoryName LIKE @categoryName";
+        results = await pool
+          .request()
+          .input("categoryName", sql.VarChar, "%" + req.query.catName + "%")
+          .query(sqlQuery);
+      } else {
+        results = await pool.request().query(sqlQuery);
+      }
+      res.write(
+        '<table border="1"><tr><th align="left"><font face="Helvetica">Add to Cart</th><th align="left"><font face="Helvetica">Product Id</th><th align="left"><font face="Helvetica">Product Name</th><th align="left"><font face="Helvetica">Product Price</th><th align="left"><font face="Helvetica">Category Name</th></tr>'
+      );
+      for (let i = 0; i < results.recordset.length; i++) {
+        let searchRes = results.recordset[i];
+        let pId = searchRes.productId;
+        let prName = searchRes.productName;
+        let prPrice = searchRes.productPrice;
+        let caName = searchRes.categoryName;
+        let color = "black";
+        if (caName == "BIOL") color = "#943131";
+        if (caName == "MATH") color = "#ad8832";
+        if (caName == "APSC") color = "#63ad32";
+        if (caName == "COSC") color = "#33917b";
+        if (caName == "POLI") color = "#338891";
+        if (caName == "WRLD") color = "#334c91";
+        res.write(
+          "<tr><td>" +
+            '<font face="Helvetica"><a href="addcart?id=' +
+            pId +
+            "&name=" +
+            prName +
+            "&price=" +
+            prPrice +
+            '">Add to Cart</a></font>' +
+            '</td><td><font face="Helvetica" color="' +
+            color +
+            '">' +
+            pId +
+            '</font></td><td><font face="Helvetica" color="' +
+            color +
+            '">' +
+            '<a href="product?id=' +
+            pId +
+            "&name=" +
+            prName +
+            "&price=" +
+            prPrice +
+            '">' +
+            prName +
+            "</a></font>" +
+            '</font></td><td><font face="Helvetica" color="' +
+            color +
+            '">' +
+            "$" +
+            prPrice +
+            '</font></td><td><font face="Helvetica" color="' +
+            color +
+            '">' +
+            caName +
+            "</font></td></tr>"
+        );
+      }
+
+      res.write("</table>");
+
+      res.end();
+    } catch (err) {
+      console.dir(err);
+      res.end();
+    }
+  })();
 });
 
 module.exports = router;
 
-
- /**
+/**
     Useful code for formatting currency:
         let num = 2.87879778;
         num = num.toFixed(2); **/
--- routes/product.js
+++ routes/product.js
@@ -1,74 +1,113 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const sql = require('mssql');
+const sql = require("mssql");
 
-router.get('/', function(req, res, next) {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<title>Edible Textbooks Orders List</title>');
-    res.write('<header><nav><ul><li><a href="/"> Home </a></li><li><a href="/listprod"> Find Products </a></li><li><a href="/showcart"> Your Cart </a></li><li> <a href="/listorder"> Past orders </a></li></ul></nav></header>');
-    res.write('<style type=text/css> header {background-color: white; position: static; left: 0; right: 0;  top: 5px;  height: 30px;  display: flex;  align-items: center;  }  header * {  display: inline;  }  header li {  margin: 20px;  }  header li a {  color: blue;  text-decoration: none;  }  </style>');
-    res.write('<h1 align="center"><font face="normal" color="#cf0921">Tasty Texts</font></h1><hr>');
-    
-    (async function() {
-        try {
-            let pool = await sql.connect(dbConfig);
-            let results = false;
-            //DONE: Use prepared Statement For Query
-            const prep = new sql.PreparedStatement(pool)
-            let sqlQuery =  "USE tempdb; SELECT productId, productName, productPrice, productImageURL, productImage FROM product ";
-            if (req.query.id && req.query.name && req.query.price) 
-                sqlQuery = sqlQuery + "WHERE productId LIKE @prodId";
-            prep.input('prodId', sql.Int)
-            prep.prepare(sqlQuery, err => {
-                console.dir(err)
-                prep.execute({prodId: req.query.id}, (err, results) => {
-                    console.dir(err)
+router.get("/", function (req, res, next) {
+  res.setHeader("Content-Type", "text/html");
+  res.write("<title>Edible Textbooks Orders List</title>");
+  res.write(
+    '<header><nav><ul><li><a href="/"> Home </a></li><li><a href="/listprod"> Find Products </a></li><li><a href="/showcart"> Your Cart </a></li><li> <a href="/listorder"> Past orders </a></li></ul></nav></header>'
+  );
+  res.write(
+    "<style type=text/css> header {background-color: white; position: static; left: 0; right: 0;  top: 5px;  height: 30px;  display: flex;  align-items: center;  }  header * {  display: inline;  }  header li {  margin: 20px;  }  header li a {  color: blue;  text-decoration: none;  }  </style>"
+  );
+  res.write(
+    '<h1 align="center"><font face="normal" color="#cf0921">Tasty Texts</font></h1><hr>'
+  );
 
-                    for(let i = 0; i <results.recordset.length; i++) {
-                        let searchRes = results.recordset[i];
-                        let pId = searchRes.productId;
-                        let prName = searchRes.productName;
-                        let prPrice = searchRes.productPrice;
-                        let prIMG_URL = searchRes.productImageURL;
-                        let prIMG_BIN = searchRes.productImage
-                        let color =  '#943131';
-                        
-                     res.write('<h1>' +'</font></h2><h2><font face="Helvetica" color="'+color+'">' + prName +'</a></font>' + '</font></h1>'); 
-                    
-                     res.write('</font><h3><font face="Helvetica">' + "Id:  " +pId + '</font></h3>');
-                    
-                     res.write('</font><h3><font face="Helvetica">' + "Price: $" + prPrice  + '</font></h3>');   
-                    // DONE: If there is a productImageURL, display using IMG tag
-                        if (prIMG_URL != null) 
-                            res.write('<img src="' + prIMG_URL + '"alt="'+ prName +'"></img>');
-                    // TODO: Retrieve any image stored directly in database. Note: Call displayImage.jsp with product id as parameter.
-                        if (prIMG_BIN != null) {
-                            res.write('<img src="displayImage.js?id=' + pId + '">')
-                        }
-                    // DONE: Add links to Add to Cart and Continue Shopping
-                    res.write('<h2><a href="addcart?id='+pId+'&name='+prName+'&price='+prPrice+'">Add to Cart</a></h2>');   
-                    res.write('<h2><a href="listprod">Continue Shopping</a></h2>');
+  (async function () {
+    try {
+      let pool = await sql.connect(dbConfig);
+      let results = false;
+      //DONE: Use prepared Statement For Query
+      const prep = new sql.PreparedStatement(pool);
+      let sqlQuery =
+        "USE tempdb; SELECT productId, productName, productPrice, productDesc, productImageURL, productImage FROM product ";
+      if (req.query.id && req.query.name && req.query.price)
+        sqlQuery = sqlQuery + "WHERE productId LIKE @prodId";
+      prep.input("prodId", sql.Int);
+      prep.prepare(sqlQuery, (err) => {
+        console.dir(err);
+        prep.execute({ prodId: req.query.id }, (err, results) => {
+          console.dir(err);
 
-                    prep.unprepare(err => {console.dir(err)
-                        res.end();
-                    })
+          for (let i = 0; i < results.recordset.length; i++) {
+            let searchRes = results.recordset[i];
+            let pId = searchRes.productId;
+            let prName = searchRes.productName;
+            let prPrice = searchRes.productPrice;
+            let prDesc = searchRes.productDesc;
+            let prIMG_URL = searchRes.productImageURL;
+            let prIMG_BIN = searchRes.productImage;
+            let color = "#943131";
 
-                    }
-                })
-                
-            })   
-           
-// Get product name to search for
-// TODO: Retrieve and display info for the product
-//  res.write('<table border="1"><tr><th align="left"><font face="Helvetica">Product Id</th><th align="left"><font face="Helvetica">Product Name</th><th align="left"><font face="Helvetica">Product Price</th></tr>');
- 
-         
-        } catch(err) {
-            console.dir(err);
-            res.write(err + "")
-            res.end();
-        }
-    })();
+            res.write(
+              "<h1>" +
+                '</font></h2><h2><font face="Helvetica" color="' +
+                color +
+                '">' +
+                prName +
+                "</a></font>" +
+                "</font></h1>"
+            );
+
+            res.write(
+              '</font><h3><font face="Helvetica">' +
+                prDesc +
+                "</font></h3>"
+            );
+
+            res.write(
+              '</font><h3><font face="Helvetica">' +
+                "Id:  " +
+                pId +
+                "</font></h3>"
+            );
+
+            res.write(
+              '</font><h3><font face="Helvetica">' +
+                "Price: $" +
+                prPrice +
+                "</font></h3>"
+            );
+            // DONE: If there is a productImageURL, display using IMG tag
+            if (prIMG_URL != null)
+              res.write(
+                '<img src="' + prIMG_URL + '"alt="' + prName + '"></img>'
+              );
+            // TODO: Retrieve any image stored directly in database. Note: Call displayImage.jsp with product id as parameter.
+            if (prIMG_BIN != null) {
+              res.write('<img src="displayImage.js?id=' + pId + '">');
+            }
+            // DONE: Add links to Add to Cart and Continue Shopping
+            res.write(
+              '<h2><a href="addcart?id=' +
+                pId +
+                "&name=" +
+                prName +
+                "&price=" +
+                prPrice +
+                '">Add to Cart</a></h2>'
+            );
+            res.write('<h2><a href="listprod">Continue Shopping</a></h2>');
+
+            prep.unprepare((err) => {
+              console.dir(err);
+              res.end();
+            });
+          }
+        });
+      });
+
+      // Get product name to search for
+      // TODO: Retrieve and display info for the product
+      //  res.write('<table border="1"><tr><th align="left"><font face="Helvetica">Product Id</th><th align="left"><font face="Helvetica">Product Name</th><th align="left"><font face="Helvetica">Product Price</th></tr>');
+    } catch (err) {
+      console.dir(err);
+      res.write(err + "");
+      res.end();
+    }
+  })();
 });
 
 module.exports = router;
