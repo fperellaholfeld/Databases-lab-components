9c68c410851dbaea1ac35c5a6231249db6559475
liamras <77363708+liamras@users.noreply.github.com>
2021-12-05 20:00:51 -0800

    Updated admin, customer, and index files

--- routes/admin.js
+++ routes/admin.js
@@ -4,19 +4,40 @@ const auth = require('../auth');
 const sql = require('mssql');
 
 router.get('/', function(req, res, next) {
+    
+    let authenticated = auth.checkAuthentication(req, res);
+    // Stop rendering the page if we aren't authenticated
+    if (!authenticated) {
+        return;
+    }
 
-	
-	// TODO: Include files auth.jsp and jdbc.jsp
-	
-	
+    let username = req.session.authenticatedUser;
 	
     res.setHeader('Content-Type', 'text/html');
-
+    res.write('<title>Tasty Texts Admin Page</title>');
+    res.write('<style type=text/css> header {background-color: white; position: static; left: 0; right: 0;  top: 5px;  height: 30px;  display: flex;  align-items: center;  }  header * {  display: inline;  }  header li {  margin: 20px;  }  header li a {  color: blue;  text-decoration: none;  }  </style>');
+    res.write('<header><nav><ul><li><a href="/"> Home </a></li><li><a href="/listprod"> Find Products </a></li><li><a href="/showcart"> Your Cart </a></li><li> <a href="/listorder"> Past orders </a></li></ul></nav></header>');
+    res.write('<h1 align="center"><font face="normal" color="#cf0921">Order List</font></h1><hr>');
+    
     (async function() {
         try {
             let pool = await sql.connect(dbConfig);
 
-	    // TODO: Write SQL query that prints out total order amount by day
+            res.write("<title>Administrator Page</title>");
+            res.write("<h3>Administrator Sales Report by Day</h3>")
+
+            let sqlQuery = "select year(orderDate) as year, month(orderDate) as month, day(orderDate) as day, SUM(totalAmount) as totalSum FROM OrderSummary GROUP BY year(orderDate), month(orderDate), day(orderDate)";
+            let result = await pool.request().query(sqlQuery);
+
+            res.write("<table class=\"table\" border=\"1\">");
+            res.write("<tr><th>Order Date</th><th>Total Order Amount</th>");
+            for (let i = 0; i < result.recordset.length; i++) {
+                let record = result.recordset[i];
+                res.write("<tr><td>" + record.year + "-" + record.month + "-" + record.day + "</td><td>$" + record.totalSum.toFixed(2) + "</td></tr>");
+            }
+            res.write("</table>");
+
+            res.end();
         } catch(err) {
             console.dir(err);
             res.write(err + "");
