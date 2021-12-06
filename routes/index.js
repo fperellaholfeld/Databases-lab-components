aef6675134fa8acd08c1a8f04442ba8ca8eaaa28
Francisco Perella-Holfeld <68356602+fperellaholfeld@users.noreply.github.com>
2021-12-05 17:46:58 -0800

    added image to home and renamed title

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function (req, res) {
     
     // TODO: Display user name that is logged in (or nothing if not logged in)	
     res.render('index', {
-        title: "YOUR NAME Grocery Main Page"
+        title: "Tasty Text Home"
         // HINT: Look at the /views/index.handlebars file
         // to get an idea of how the index page is being rendered
     });
