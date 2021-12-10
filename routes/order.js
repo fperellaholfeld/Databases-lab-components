e33c5d29b72871b9abedf67b51939a76d6bad893
liamras <77363708+liamras@users.noreply.github.com>
2021-12-09 21:09:47 -0800

    added header to checkout, added header to order

--- routes/order.js
+++ routes/order.js
@@ -5,6 +5,10 @@ const moment = require("moment");
 
 router.get("/", function (req, res, next) {
   res.setHeader("Content-Type", "text/html");
+  res.write('<header><nav><ul><li><a href="/"> Home </a></li><li><a href="/listprod"> Find Products </a></li><li><a href="/showcart"> Your Cart </a></li><li> <a href="/listorder"> Past orders </a></li></ul></nav></header>');
+  res.write("<style type=text/css> header {background-color: white; position: static; left: 0; right: 0;  top: 5px;  height: 30px;  display: flex;  align-items: center;  }  header * {  display: inline;  }  header li {  margin: 20px;  }  header li a {  color: blue;  text-decoration: none;  }  </style>");
+  res.write('<h1 align="center"><font face="normal" color="#cf0921">Tasty Texts</font></h1><hr>');
+    
   res.write("<title>Tasty Texts</title>");
 
   let custId = false;
